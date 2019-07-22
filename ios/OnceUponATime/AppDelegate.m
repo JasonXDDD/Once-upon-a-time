4db092950b47d8a381d000429ed61d220fa237a6
Len_AshBell <jason1022liu@gmail.com>
2019-07-23 00:26:35 +0800

    ios build

--- ios/OnceUponATime/AppDelegate.m
+++ ios/OnceUponATime/AppDelegate.m
@@ -32,11 +32,6 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
-#endif
 }
-
 @end
