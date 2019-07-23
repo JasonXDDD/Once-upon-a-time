4dc377c68f7c991fb00eff0c172c706d398cd869
Len_AshBell <jason1022liu@gmail.com>
2019-07-24 06:55:09 +0800

    [editStory] update countDown

--- src/pages/editStory.js
+++ src/pages/editStory.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Platform, ImageBackground, Dimensions } from "react-native";
+import { Platform, ImageBackground, Dimensions, Text, StyleSheet } from "react-native";
 import { inject, observer } from "mobx-react";
 
 import EditStory_BG from "../assets/images/EditStory/BG.png";
@@ -14,6 +14,13 @@ import StoryToolRecord from "../components/story/storyToolRecord";
 import GoBack from "../components/record/goBack";
 import RecordTool from "../components/record/recordTool";
 import SceneTool from "../components/scene/sceneTool";
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+const COUNT_WIDTH = 160;
+const COUNT_HEIGHT = 240;
+const COUNT_RIGHT = screenWidth / 2 - COUNT_WIDTH / 2;
+const COUNT_TOP = screenHeight / 2 - COUNT_HEIGHT / 2 - screenHeight * 0.06;
 
 @inject("rootStore")
 @observer
@@ -43,7 +50,18 @@ export default class EditStory extends React.Component {
         {/* when record */}
         <GoBack navigation={this.props.navigation} />
         <RecordTool navigation={this.props.navigation} />
+
+        <Text style={[styles.countText, { display: this.storyStore.count == 0? 'none': 'flex' }]}>{this.storyStore.count}</Text>
       </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  countText: {
+    fontSize: 200, 
+    position: 'absolute', 
+    top: COUNT_TOP, 
+    right: COUNT_RIGHT
+  }
+});
