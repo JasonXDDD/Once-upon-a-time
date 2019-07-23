d5a5f991be592075e2717434be45d1710ed1e76f
Len_AshBell <jason1022liu@gmail.com>
2019-07-24 02:29:33 +0800

    [boxTool] update share, delete fn

--- src/components/box/boxTool.js
+++ src/components/box/boxTool.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Image, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 import { inject, observer } from 'mobx-react'
 import Share from 'react-native-share';
+import RNFS from 'react-native-fs';
 
 import Btn_Share from '../../assets/images/StoryBox/Btn_share.png'
 import Btn_Delete from '../../assets/images/StoryBox/Btn_delete.png'
@@ -25,6 +26,17 @@ export default class BoxTool extends Component {
     this.navigation = props.navigation
   }
 
+  deleteVideo(path){
+    return RNFS.unlink(path)
+    .then(() => {
+      console.log('FILE DELETED');
+    })
+    // `unlink` will throw an error, if the item to unlink does not exist
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }
+
   render() {
     return (
       <View style={[styles.recordTool]}>
@@ -32,13 +44,16 @@ export default class BoxTool extends Component {
 
         <TouchableOpacity 
           onPress={() => {
+            this.deleteVideo(this.props.selectVideo.video)
           }}>
           <Image style={styles.recordIcon} source={Btn_Delete}></Image>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => {
-            Share.open()
+            Share.open({
+              url: this.props.selectVideo.video
+            })
             .then((res) => { console.log(res) })
             .catch((err) => { err && console.log(err); });
           }}>
