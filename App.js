28d952bdf47eb4fe481987114d4bf45e3a7f4f31
Len_AshBell <jason1022liu@gmail.com>
2019-10-08 14:43:06 +0800

    update siri button UI & remove snapshot

--- App.js
+++ App.js
@@ -6,23 +6,6 @@ import { Provider, observer } from "mobx-react";
 import { Provider as ProviderAntd } from "@ant-design/react-native";
 import * as store from "./src/stores/index";
 import Sound from 'react-native-sound';
-import { SiriShortcutsEvent, donateShortcut, suggestShortcuts, clearAllShortcuts, clearShortcutsWithIdentifiers, presentShortcut } from "react-native-siri-shortcut";
-import AddToSiriButton, { SiriButtonStyles, supportsSiriButton } from "react-native-siri-shortcut/AddToSiriButton";
-
-
-const opts1: ShortcutOptions = {
-  activityType: "io.github.jasonxddd.sayHello",
-  title: "從前從前說故事",
-  description: "拉拉",
-  userInfo: { say: 'story' },
-  keywords: ["故事", "從前"],
-  persistentIdentifier: "io.github.jasonxddd.sayHello",
-  isEligibleForSearch: true,
-  isEligibleForPrediction: true,
-  suggestedInvocationPhrase: "講故事給我聽",
-  needsSave: true,
-};
-
 
 @observer
 export default class App extends Component<Props> {  
@@ -35,13 +18,6 @@ export default class App extends Component<Props> {
     setTimeout(() => {
       store.soundStore.playMusic(store.soundStore.bgmPlayer, 0.4, -1)
     }, 1000)
-
-    // set sirikit listener
-    SiriShortcutsEvent.addListener("SiriShortcutListener", ({userInfo, activityType}) => {
-      store.storyStore.shortcutInfo = JSON.stringify(userInfo)
-      store.storyStore.shortcutActivityType = activityType
-    });
-    suggestShortcuts([opts1]);
   }
 
   switchBGM(){
@@ -68,19 +44,6 @@ export default class App extends Component<Props> {
               <Text>{store.soundStore.isBgm? 'Close BGM': 'Open BGM'}</Text>
             </TouchableOpacity>
 
-            {supportsSiriButton && (
-              <AddToSiriButton
-                style={styles.siriButton}
-                buttonStyle={SiriButtonStyles.blackOutline}
-                onPress={() => {
-                  presentShortcut(opts1, ({ status }) => {
-                    console.log(`I was ${status}`);
-                  });
-                }}
-                shortcut={opts1}
-              />
-            )}
-
           </SafeAreaView>
         </ProviderAntd>
       </Provider>
@@ -97,12 +60,5 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     top: 50,
     right: 50
-  },
-
-  siriButton: {
-    position: "absolute",
-    overflow: "hidden",
-    bottom: 25,
-    left: 5
   }
 })
