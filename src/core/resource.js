7a52421cfa6aaee8038c6f2c69964a1dfdf0bb5c
Len_AshBell <jason1022liu@gmail.com>
2020-02-25 05:04:55 +0800

    update video to jasonxddd server

--- src/core/resource.js
+++ src/core/resource.js
@@ -1,162 +1,163 @@
 // draw sticker
-import DrawStory_BG from "../assets/images/DrawStory/BG.png";
+import DrawStory_BG from '../assets/images/DrawStory/BG.png'
 
 // edit story
-import EditStory_BG from "../assets/images/EditStory/BG.png";
-import RecordStory_BG from "../assets/images/RecordStory/BG.png";
-import Redo from "../assets/images/EditStory/btn_redo.png";
-import Teaching from "../assets/images/EditStory/Teaching.png";
+import EditStory_BG from '../assets/images/EditStory/BG.png'
+import RecordStory_BG from '../assets/images/RecordStory/BG.png'
+import Redo from '../assets/images/EditStory/btn_redo.png'
+import Teaching from '../assets/images/EditStory/Teaching.png'
 
 // story box
 import StoryBox_BG from '../assets/images/StoryBox/BG.png'
 
 // tabbar
-import TabBar_BG from "../assets/images/TabBar/BG.png";
-import EditStorySelected from "../assets/images/TabBar/EditStory_Selected.png";
-import EditStoryUnselected from "../assets/images/TabBar/EditStory_Unselected.png";
-import DrawStorySelected from "../assets/images/TabBar/DrawStory_Selected.png";
-import DrawStoryUnselected from "../assets/images/TabBar/DrawStory_Unselected.png";
-import StoryBoxSelected from "../assets/images/TabBar/StoryBox_Selected.png";
-import StoryBoxUnselected from "../assets/images/TabBar/StoryBox_Unselected.png";
+import TabBar_BG from '../assets/images/TabBar/BG.png'
+import EditStorySelected from '../assets/images/TabBar/EditStory_Selected.png'
+import EditStoryUnselected from '../assets/images/TabBar/EditStory_Unselected.png'
+import DrawStorySelected from '../assets/images/TabBar/DrawStory_Selected.png'
+import DrawStoryUnselected from '../assets/images/TabBar/DrawStory_Unselected.png'
+import StoryBoxSelected from '../assets/images/TabBar/StoryBox_Selected.png'
+import StoryBoxUnselected from '../assets/images/TabBar/StoryBox_Unselected.png'
 
 // toolbar
-import ToolBar_Tap from "../assets/images/img_Topmenu.png";
+import ToolBar_Tap from '../assets/images/img_Topmenu.png'
 
 // story board
-import Teaching_BG from '../assets/images/EditStory/Teaching.png';
+import Teaching_BG from '../assets/images/EditStory/Teaching.png'
 
 // story  tool record
-import Btn_Recording from "../assets/images/RecordStory/Btn_Recording.png";
+import Btn_Recording from '../assets/images/RecordStory/Btn_Recording.png'
 
 // scene tool
-import Movie_Selected from "../assets/images/EditStory/btn_movie_Selected.png";
-import Movie_Unselected from "../assets/images/EditStory/btn_movie_Unselected.png";
-import BG_Scene_V from "../assets/images/EditStory/BG_ScenesBar_v.png";
-import BG_Scene_H from "../assets/images/EditStory/BG_ScenesBar_h.png";
-import Btn_NumberBackground from "../assets/images/EditStory/Btn_SceneNumberBackground.png";
-import Btn_NumberBackground_Selected from "../assets/images/EditStory/Btn_SceneNumberBackground_selected.png";
-import Btn_AddScene from "../assets/images/EditStory/Btn_AddScene.png";
+import Movie_Selected from '../assets/images/EditStory/btn_movie_Selected.png'
+import Movie_Unselected from '../assets/images/EditStory/btn_movie_Unselected.png'
+import BG_Scene_V from '../assets/images/EditStory/BG_ScenesBar_v.png'
+import BG_Scene_H from '../assets/images/EditStory/BG_ScenesBar_h.png'
+import Btn_NumberBackground from '../assets/images/EditStory/Btn_SceneNumberBackground.png'
+import Btn_NumberBackground_Selected from '../assets/images/EditStory/Btn_SceneNumberBackground_selected.png'
+import Btn_AddScene from '../assets/images/EditStory/Btn_AddScene.png'
 
 // go back
-import Btn_Edit from "../assets/images/RecordStory/Btn_Edit.png";
+import Btn_Edit from '../assets/images/RecordStory/Btn_Edit.png'
 
 // music board
-import BG_Music from "../assets/images/RecordStory/BG_Music.png";
+import BG_Music from '../assets/images/RecordStory/BG_Music.png'
 
 // record tool
 import Btn_Recording_Stop from '../assets/images/RecordStory/Btn_Recording_Stop.png'
 
 // draw board
-import DrawBoard_IMG from "../assets/images/DrawStory/drawBoard.png";
-import Reback from "../assets/images/DrawStory/btn_reback.png";
-import Eraser from "../assets/images/DrawStory/btn_eraser.png";
-import Plus from "../assets/images/DrawStory/btn_penPlus.png";
-import Delete from "../assets/images/StoryBox/Btn_delete.png";
-import Camera from "../assets/images/DrawStory/btn_camera.png";
+import DrawBoard_IMG from '../assets/images/DrawStory/drawBoard.png'
+import Reback from '../assets/images/DrawStory/btn_reback.png'
+import Eraser from '../assets/images/DrawStory/btn_eraser.png'
+import Plus from '../assets/images/DrawStory/btn_penPlus.png'
+import Delete from '../assets/images/StoryBox/Btn_delete.png'
+import Camera from '../assets/images/DrawStory/btn_camera.png'
 
-import c6BCCF7 from "../assets/images/DrawStory/color/btn_page03_Aqua_blue.png";
-import s6BCCF7 from "../assets/images/DrawStory/color/btn_page03_Aqua_blue_selected.png";
-import c000000 from "../assets/images/DrawStory/color/btn_page03_black.png";
-import s000000 from "../assets/images/DrawStory/color/btn_page03_black_selected.png";
-import c2367C0 from "../assets/images/DrawStory/color/btn_page03_blue.png";
-import s2367C0 from "../assets/images/DrawStory/color/btn_page03_blue_selected.png";
-import c84574F from "../assets/images/DrawStory/color/btn_page03_Brown.png";
-import s84574F from "../assets/images/DrawStory/color/btn_page03_Brown_selected.png";
-import c49C37C from "../assets/images/DrawStory/color/btn_page03_green.png";
-import s49C37C from "../assets/images/DrawStory/color/btn_page03_green_selected.png";
-import cFF7D1C from "../assets/images/DrawStory/color/btn_page03_orange.png";
-import sFF7D1C from "../assets/images/DrawStory/color/btn_page03_orange_selected.png";
-import cFF75B5 from "../assets/images/DrawStory/color/btn_page03_Pink.png";
-import sFF75B5 from "../assets/images/DrawStory/color/btn_page03_Pink_selected.png";
-import c9323A1 from "../assets/images/DrawStory/color/btn_page03_purple.png";
-import s9323A1 from "../assets/images/DrawStory/color/btn_page03_purple_selected.png";
-import cFF403F from "../assets/images/DrawStory/color/btn_page03_Red.png";
-import sFF403F from "../assets/images/DrawStory/color/btn_page03_Red_selected.png";
-import cFFD298 from "../assets/images/DrawStory/color/btn_page03_skin_color.png";
-import sFFD298 from "../assets/images/DrawStory/color/btn_page03_skin_color_selected.png";
-import cFFFFFF from "../assets/images/DrawStory/color/btn_page03_white.png";
-import sFFFFFF from "../assets/images/DrawStory/color/btn_page03_white_selected.png";
-import cFFFF57 from "../assets/images/DrawStory/color/btn_page03_yellow.png";
-import sFFFF57 from "../assets/images/DrawStory/color/btn_page03_yellow_selected.png";
+import c6BCCF7 from '../assets/images/DrawStory/color/btn_page03_Aqua_blue.png'
+import s6BCCF7 from '../assets/images/DrawStory/color/btn_page03_Aqua_blue_selected.png'
+import c000000 from '../assets/images/DrawStory/color/btn_page03_black.png'
+import s000000 from '../assets/images/DrawStory/color/btn_page03_black_selected.png'
+import c2367C0 from '../assets/images/DrawStory/color/btn_page03_blue.png'
+import s2367C0 from '../assets/images/DrawStory/color/btn_page03_blue_selected.png'
+import c84574F from '../assets/images/DrawStory/color/btn_page03_Brown.png'
+import s84574F from '../assets/images/DrawStory/color/btn_page03_Brown_selected.png'
+import c49C37C from '../assets/images/DrawStory/color/btn_page03_green.png'
+import s49C37C from '../assets/images/DrawStory/color/btn_page03_green_selected.png'
+import cFF7D1C from '../assets/images/DrawStory/color/btn_page03_orange.png'
+import sFF7D1C from '../assets/images/DrawStory/color/btn_page03_orange_selected.png'
+import cFF75B5 from '../assets/images/DrawStory/color/btn_page03_Pink.png'
+import sFF75B5 from '../assets/images/DrawStory/color/btn_page03_Pink_selected.png'
+import c9323A1 from '../assets/images/DrawStory/color/btn_page03_purple.png'
+import s9323A1 from '../assets/images/DrawStory/color/btn_page03_purple_selected.png'
+import cFF403F from '../assets/images/DrawStory/color/btn_page03_Red.png'
+import sFF403F from '../assets/images/DrawStory/color/btn_page03_Red_selected.png'
+import cFFD298 from '../assets/images/DrawStory/color/btn_page03_skin_color.png'
+import sFFD298 from '../assets/images/DrawStory/color/btn_page03_skin_color_selected.png'
+import cFFFFFF from '../assets/images/DrawStory/color/btn_page03_white.png'
+import sFFFFFF from '../assets/images/DrawStory/color/btn_page03_white_selected.png'
+import cFFFF57 from '../assets/images/DrawStory/color/btn_page03_yellow.png'
+import sFFFF57 from '../assets/images/DrawStory/color/btn_page03_yellow_selected.png'
 
 // box tool
-import Btn_Share from "../assets/images/StoryBox/Btn_share.png";
-import Btn_Delete from "../assets/images/StoryBox/Btn_delete.png";
-import Btn_Reload from "../assets/images/StoryBox/Btn_reload.png";
-import Btn_Siri from "../assets/images/StoryBox/Btn_siri.png";
+import Btn_Share from '../assets/images/StoryBox/Btn_share.png'
+import Btn_Delete from '../assets/images/StoryBox/Btn_delete.png'
+import Btn_Reload from '../assets/images/StoryBox/Btn_reload.png'
+import Btn_Siri from '../assets/images/StoryBox/Btn_siri.png'
 
 // box store
-import Sample_Video from '../assets/sample.mp4'
+// import Sample_Video from '../assets/sample.mp4'
 import Sample_Image from '../assets/images/sample-video.png'
+const Sample_Vedio_URL = 'http://jasonxddd.nctu.me:8001/sample.mp4'
 
 // sound store
-import Background_Music from "../assets/sound/Rainbow_Forest.mp3";
-import TabBar_Click from "../assets/sound/button-tab.mp3";
-import Button_Click from "../assets/sound/button-click.mp3";
-import Delete_Click from "../assets/sound/button-delete.mp3";
-import Pop_Click from "../assets/sound/button-pop.wav";
-import Long_Press_Click from "../assets/sound/button-long-press.mp3";
-import Bubble from "../assets/sound/bubble.wav";
-import ToolBar_Click from "../assets/sound/bloop-noise.wav";
-import DrawColor_Click from "../assets/sound/bloop-noise.wav";
-import BGM_Open from "../assets/sound/bgm-open.wav";
-import BGM_Close from "../assets/sound/bgm-close.wav";
+import Background_Music from '../assets/sound/Rainbow_Forest.mp3'
+import TabBar_Click from '../assets/sound/button-tab.mp3'
+import Button_Click from '../assets/sound/button-click.mp3'
+import Delete_Click from '../assets/sound/button-delete.mp3'
+import Pop_Click from '../assets/sound/button-pop.wav'
+import Long_Press_Click from '../assets/sound/button-long-press.mp3'
+import Bubble from '../assets/sound/bubble.wav'
+import ToolBar_Click from '../assets/sound/bloop-noise.wav'
+import DrawColor_Click from '../assets/sound/bloop-noise.wav'
+import BGM_Open from '../assets/sound/bgm-open.wav'
+import BGM_Close from '../assets/sound/bgm-close.wav'
 
-import g3 from "../assets/sound/piano/g3.wav";
-import a3 from "../assets/sound/piano/a3.wav";
-import b3 from "../assets/sound/piano/b3.wav";
-import c4 from "../assets/sound/piano/c4.wav";
-import d4 from "../assets/sound/piano/d4.wav";
-import e4 from "../assets/sound/piano/e4.wav";
-import f4 from "../assets/sound/piano/f4.wav";
-import g4 from "../assets/sound/piano/g4.wav";
-import a4 from "../assets/sound/piano/a4.wav";
-import b4 from "../assets/sound/piano/b4.wav";
-import c5 from "../assets/sound/piano/c5.wav";
-import d5 from "../assets/sound/piano/d5.wav";
+import g3 from '../assets/sound/piano/g3.wav'
+import a3 from '../assets/sound/piano/a3.wav'
+import b3 from '../assets/sound/piano/b3.wav'
+import c4 from '../assets/sound/piano/c4.wav'
+import d4 from '../assets/sound/piano/d4.wav'
+import e4 from '../assets/sound/piano/e4.wav'
+import f4 from '../assets/sound/piano/f4.wav'
+import g4 from '../assets/sound/piano/g4.wav'
+import a4 from '../assets/sound/piano/a4.wav'
+import b4 from '../assets/sound/piano/b4.wav'
+import c5 from '../assets/sound/piano/c5.wav'
+import d5 from '../assets/sound/piano/d5.wav'
 
 // tool store
-import BtnSceneUnselected from "../assets/images/EditStory/Btn_Material_Unselected.png";
-import BtnSceneSelected from "../assets/images/EditStory/Btn_Material_Selected.png";
-import BtnCharacterUnselected from "../assets/images/EditStory/Btn_Character_Unselected.png";
-import BtnCharacterSelected from "../assets/images/EditStory/Btn_Character_Selected.png";
-import BtnStickerUnselected from "../assets/images/EditStory/Btn_Sticker_Unselected.png";
-import BtnStickerSelected from "../assets/images/EditStory/Btn_Sticker_Selected.png";
-import BtnMusicSelected from "../assets/images/EditStory/Btn_Music_Selected.png";
-import BtnMusicUnselected from "../assets/images/EditStory/Btn_Music_Unselected.png";
+import BtnSceneUnselected from '../assets/images/EditStory/Btn_Material_Unselected.png'
+import BtnSceneSelected from '../assets/images/EditStory/Btn_Material_Selected.png'
+import BtnCharacterUnselected from '../assets/images/EditStory/Btn_Character_Unselected.png'
+import BtnCharacterSelected from '../assets/images/EditStory/Btn_Character_Selected.png'
+import BtnStickerUnselected from '../assets/images/EditStory/Btn_Sticker_Unselected.png'
+import BtnStickerSelected from '../assets/images/EditStory/Btn_Sticker_Selected.png'
+import BtnMusicSelected from '../assets/images/EditStory/Btn_Music_Selected.png'
+import BtnMusicUnselected from '../assets/images/EditStory/Btn_Music_Unselected.png'
 
-import Forest from "../assets/images/scene/Forest.png";
-import Room from "../assets/images/scene/Room.png";
-import Outside from "../assets/images/scene/Outside.png";
-import CameraItem from "../assets/images/scene/Camera.png";
-import sLock01 from "../assets/images/scene/lock01.png";
-import sLock02 from "../assets/images/scene/lock02.png";
-import sLock03 from "../assets/images/scene/lock03.png";
+import Forest from '../assets/images/scene/Forest.png'
+import Room from '../assets/images/scene/Room.png'
+import Outside from '../assets/images/scene/Outside.png'
+import CameraItem from '../assets/images/scene/Camera.png'
+import sLock01 from '../assets/images/scene/lock01.png'
+import sLock02 from '../assets/images/scene/lock02.png'
+import sLock03 from '../assets/images/scene/lock03.png'
 
-import BigMonster from "../assets/images/character/BigMonster.png";
-import SmallMoster from "../assets/images/character/smallMoster.png";
-import BigMonsterAnimate from "../assets/images/character/BigMonsterAnimate.gif";
-import SmallMosterAnimate from "../assets/images/character/smallMosterAnimate.gif";
-import Sun from "../assets/images/character/Sun.png";
-import Redhat from "../assets/images/character/redhat.png";
-import Hunter from "../assets/images/character/hunter.png";
-import Bee from "../assets/images/character/bee.png";
-import Grandma from "../assets/images/character/grandma.png";
-import Wolf from "../assets/images/character/wolf.png";
-import People1 from "../assets/images/character/people1.png";
-import People2 from "../assets/images/character/people2.png";
-import People3 from "../assets/images/character/people3.png";
-import People4 from "../assets/images/character/people4.png";
-import cLock01 from "../assets/images/character/lock01.png";
+import BigMonster from '../assets/images/character/BigMonster.png'
+import SmallMoster from '../assets/images/character/smallMoster.png'
+import BigMonsterAnimate from '../assets/images/character/BigMonsterAnimate.gif'
+import SmallMosterAnimate from '../assets/images/character/smallMosterAnimate.gif'
+import Sun from '../assets/images/character/Sun.png'
+import Redhat from '../assets/images/character/redhat.png'
+import Hunter from '../assets/images/character/hunter.png'
+import Bee from '../assets/images/character/bee.png'
+import Grandma from '../assets/images/character/grandma.png'
+import Wolf from '../assets/images/character/wolf.png'
+import People1 from '../assets/images/character/people1.png'
+import People2 from '../assets/images/character/people2.png'
+import People3 from '../assets/images/character/people3.png'
+import People4 from '../assets/images/character/people4.png'
+import cLock01 from '../assets/images/character/lock01.png'
 
-import stickerSample from "../assets/images/sticker-sample.png";
-import musicItem from "../assets/images/music-item.png";
+import stickerSample from '../assets/images/sticker-sample.png'
+import musicItem from '../assets/images/music-item.png'
 
-import Happy from "../assets/sound/scene/Rainbow_Forest.mp3";
-import Desert from "../assets/sound/scene/Desert_Caravan.mp3";
-import War from "../assets/sound/scene/Eyes_of_Glory.mp3";
-import Suspense from "../assets/sound/scene/Carol_of_the_Bells.mp3";
-import Thunder from "../assets/sound/scene/Thunder_Crack.mp3";
+import Happy from '../assets/sound/scene/Rainbow_Forest.mp3'
+import Desert from '../assets/sound/scene/Desert_Caravan.mp3'
+import War from '../assets/sound/scene/Eyes_of_Glory.mp3'
+import Suspense from '../assets/sound/scene/Carol_of_the_Bells.mp3'
+import Thunder from '../assets/sound/scene/Thunder_Crack.mp3'
 
 export const RES = {
   DrawStory_BG: DrawStory_BG,
@@ -219,7 +220,7 @@ export const RES = {
   Btn_Delete: Btn_Delete,
   Btn_Reload: Btn_Reload,
   Btn_Siri: Btn_Siri,
-  Sample_Video: Sample_Video,
+  Sample_Video: { uri: Sample_Vedio_URL },
   Sample_Image: Sample_Image,
   Background_Music: Background_Music,
   TabBar_Click: TabBar_Click,
@@ -280,5 +281,5 @@ export const RES = {
   Desert: Desert,
   War: War,
   Suspense: Suspense,
-  Thunder: Thunder,
+  Thunder: Thunder
 }
